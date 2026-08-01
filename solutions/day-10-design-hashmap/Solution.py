a834a49c3eab484b335ffbe2b7ce880c3a277665
Sairam BN <bnsairam14@gmail.com>
2026-08-01 23:51:07 +0530

    Day 10: Design HashMap - Python solution

---
+++ solutions/day-10-design-hashmap/Solution.py
@@ -0,0 +1,30 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def _hash(self, key: int) -> int:
+        return key % self.size
+
+    def put(self, key: int, value: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, value)
+                return
+        bucket.append((key, value))
+
+    def get(self, key: int) -> int:
+        bucket = self.buckets[self._hash(key)]
+        for k, v in bucket:
+            if k == key:
+                return v
+        return -1
+
+    def remove(self, key: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket.pop(i)
+                return
