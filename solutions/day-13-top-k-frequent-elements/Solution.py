959e9f0637c8c5cdc71207ce12e282ecdd7d88be
Sairam BN <bnsairam14@gmail.com>
2026-08-02 00:02:17 +0530

    Day 13: Top K Frequent Elements - Python solution

---
+++ solutions/day-13-top-k-frequent-elements/Solution.py
@@ -0,0 +1,8 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        return [num for num, _ in count.most_common(k)]
