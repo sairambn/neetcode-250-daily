ef70fa90b4ea5140f393aeddc9d0c65c5f193d6a
Sairam BN <bnsairam14@gmail.com>
2026-08-01 23:50:08 +0530

    Day 6: Group Anagrams - C++ solution

---
+++ solutions/day-06-group-anagrams/Solution.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> groups;
+
+        for (const auto& s : strs) {
+            std::string key = s;
+            std::sort(key.begin(), key.end());
+            groups[key].push_back(s);
+        }
+
+        std::vector<std::vector<std::string>> result;
+        for (auto& pair : groups) {
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
