c2f38f0934ce102f3c44517f0fcd5af763363c68
Sairam BN <bnsairam14@gmail.com>
2026-08-01 23:50:33 +0530

    Day 8: Majority Element - Java solution

---
+++ solutions/day-08-majority-element/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Integer candidate = null;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
