de6c772a669f35dd557aabd0441768615c60697b
Sairam BN <bnsairam14@gmail.com>
2026-08-01 23:50:44 +0530

    Day 9: Design HashSet - Python solution

---
+++ solutions/day-09-design-hashset/Solution.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def _hash(self, key: int) -> int:
+        return key % self.size
+
+    def add(self, key: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        if key not in bucket:
+            bucket.append(key)
+
+    def remove(self, key: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        if key in bucket:
+            bucket.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.buckets[self._hash(key)]
