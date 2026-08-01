a73752703a8043a67e88a6442280bd7b3ea6d4ad
Sairam BN <bnsairam14@gmail.com>
2026-08-02 00:02:40 +0530

    Day 14: Encode and Decode Strings - Python solution

---
+++ solutions/day-14-encode-and-decode-strings/Solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        return "".join(f"{len(s)}#{s}" for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i = 0
+        while i < len(s):
+            j = s.find("#", i)
+            length = int(s[i:j])
+            result.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return result
